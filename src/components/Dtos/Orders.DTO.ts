29c3547c108a200d1a9ccb1a356d36fc248658fd
RovarQRoom <rovar.07000120@outlook.com>
2023-04-18 12:46:10 +0300

    Fixed Orders DTO

--- src/components/Dtos/Orders.DTO.ts
+++ src/components/Dtos/Orders.DTO.ts
@@ -6,7 +6,7 @@ export class OrderDTO {
     status: string;
     createdAt: Date;
     deletedAt: Date | null = null;
-    constructor(userid: string, item:ItemDto[], status:string,createdAt: Date, deletedAt: Date | null = null) {
+    constructor(userid: string, item:ItemDto[], status:string, createdAt: Date, deletedAt: Date | null = null) {
         this.userid = userid;
         this.item = item;
         this.status = status;
